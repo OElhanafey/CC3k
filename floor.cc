62e0d9dc6f8c58bac8b1cf028057c728e6a7854b
cshah7 <chakshu711@gmail.com>
2016-12-01 22:37:21 -0500

    Re-added generate

--- floor.cc
+++ floor.cc
@@ -59,9 +59,119 @@ bool Floor::isCellValid(int r, int c, bool isPlayer) {
 	}
 }
 
+void Floor::generate() {
+    //Player
+    bool generated = false;
+    int playerChamber = std::rand() % 5;
+    while(!generated) {
+        int array = std::rand() % chamber[playerChamber].size();
+        if(chamber[playerChamber]./*Access array*/.isPlayerValid()) {
+            //Generate Player
+            generated = true;
+        }
+    }
+    //Stairs
+    generated = false;
+    while(!generated) {
+        int chamber = std::rand() % 5;
+        if(chamber != playerChamber) {
+            generated = true;
+        }
+    }
+    generated = false;
+    while(!generated) {
+        array = std::rand() % chamber[chamber].size();
+        if(chamber[chamber]./*Access array*/.isPlayerValid()) {
+            //Generate stairs
+            generated = true;
+        }
+    }
+    //Potions
+    int amountGen = 0;
+    while(amountGen < 10) {
+        int type = std::rand() % 6 + 1;
+        chamber = std::rand() % 5;
+        array = std::rand() % chamber[chamber].size();
+        if(chamber[chamber]/*Access array*/.isEnemyValid()) {
+            if(type == 1) {
+                //Restore Health
+            }
+            else if(type == 2) {
+                //Boost Atk
+            }
+            else if(type == 3) {
+                //Boost Def
+            }
+            else if(type == 4) {
+                //Poison health
+            }
+            else if(type == 5) {
+                //Wound Atk
+            }
+            else {
+                //Wound Def
+            }
+            ++amountGen;
+        }
+    }
+    //Gold
+    amountGen = 0;
+    while(amountGen < 10) {
+        type = std::rand() % 8 + 1;
+        chamber = std::rand() % 5;
+        array = std::rand() % chamber[chamber].size();
+        if(chamber[chamber]/*Access array*/.isEnemyValid()) {
+            if(type >= 1 && type <= 5) {
+                GameObject* gold = new Gold(2);
+            }
+            else if(type == 6) {
+                GameObject* gold = new Gold(6);
+            }
+            else {
+                GameObject* gold = new Gold(1);
+            }
+            int x,y; //GET Coordinates
+            objectAdd(x,y,gold);
+            // setting the dragonhoard to true if the gold type is dragon gold.
+            if(gold->getGold() == 6){
+                setDragonHoard(x,y,true);
+            }
+            ++amountGen
+        }
+    }
+    //Enemy
+    amountGen = 0;
+    while(amountGen < 20) {
+        type = std::rand() % 18 + 1;
+        chamber = std::rand() % 55;
+        array = std::rand() % chamber[chamber].size();
+        if(chamber[chamber]/*Access array*/.isEnemyValid()) {
+            if(type >= 1 && type <= 4) {
+                //Human
+            }
+            else if(type >= 5 <= 7) {
+                //Dwarf
+            }
+            else if(type >= 8 && type <= 12) {
+                //Halfling
+            }
+            else if(type >= 13 && type <= 14) {
+                //Elf
+            }
+            else if(type >= 15 && type <= 16) {
+                //Orc
+            }
+            else if(type >= 17 && type <= 18) {
+                //Merchant
+            }
+            ++amountGen;
+        }
+    }
+}
+
 void Floor::print() {
-	for(unsigned int i=0; i<grid.size(); ++i) {
-		for(unsigned int j=0; j<grid[i].size(); ++j) {
+    for(unsigned int i=0; i<grid.size(); ++i) {
+        for(unsigned int j=0; j<grid[i].size(); ++j) {
 			std::cout << grid[i][j].getSymbol();
 		}
 		std::cout << std::endl;
